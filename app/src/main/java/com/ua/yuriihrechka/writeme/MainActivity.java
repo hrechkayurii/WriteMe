9263f04ec59c1c8fa5a29819ace7aeece0a60639
ukropBandera <ukrop.bandera@gmail.com>
2019-01-08 22:40:01 +0200

    test1

---
+++ app/src/main/java/com/ua/yuriihrechka/writeme/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ua.yuriihrechka.writeme;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TabsAccessorAdapter mTabsAccessorAdapter;
+
+    // fb
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WriteMe");
+
+        mViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        mTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabsAccessorAdapter);
+
+        mTabLayout = (TabLayout)findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null){
+            sendUserToLoginActivity();
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+}
