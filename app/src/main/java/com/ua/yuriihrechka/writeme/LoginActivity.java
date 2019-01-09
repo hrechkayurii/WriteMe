ae68ae15766cdf976be4312cfd521020471edf16
ukropBandera <ukrop.bandera@gmail.com>
2019-01-09 16:09:43 +0200

    20190109_7

--- app/src/main/java/com/ua/yuriihrechka/writeme/LoginActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/writeme/LoginActivity.java
@@ -3,10 +3,19 @@ package com.ua.yuriihrechka.writeme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink;
+
 
     // fb
     private FirebaseUser currentUser;
@@ -15,6 +24,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitialzeFields();
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void InitialzeFields() {
+
+        loginButton = (Button)findViewById(R.id.login_button);
+        userEmail = (EditText)findViewById(R.id.login_email);
+        userPassword = (EditText)findViewById(R.id.login_password);
+        needNewAccountLink = (TextView)findViewById(R.id.need_new_account_link);
     }
 
     @Override
@@ -30,5 +56,11 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(mainIntent);
+    }
+
+    private void sendUserToRegisterActivity() {
+
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
