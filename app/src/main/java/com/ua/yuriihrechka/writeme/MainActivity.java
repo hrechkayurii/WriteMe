fb26617ad21ebf7054ec14cb5fa0fd4b40272dc7
ukropBandera <ukrop.bandera@gmail.com>
2019-01-10 12:52:13 +0200

    20190110_02

--- app/src/main/java/com/ua/yuriihrechka/writeme/MainActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/writeme/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.TabLayout;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -55,5 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.main_logout_option){
+            mAuth.signOut();
+            sendUserToLoginActivity();
+        }
+
+        if (item.getItemId() == R.id.main_settings_option){
+
+        }
+
+        if (item.getItemId() == R.id.main_find_friends_option){
+
+        }
+
+        return true;
     }
 }
