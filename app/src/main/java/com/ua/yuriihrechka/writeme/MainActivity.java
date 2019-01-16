0bbe6a56b535657f8e89135823f678e8979dbee2
ukropBandera <ukrop.bandera@gmail.com>
2019-01-16 17:27:11 +0200

    25_5

--- app/src/main/java/com/ua/yuriihrechka/writeme/MainActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/writeme/MainActivity.java
@@ -25,6 +25,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+//20190116
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
