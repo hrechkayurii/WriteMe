f0fa4a0159c9c1cd61a6a5932fcff8779fc4a158
ukropBandera <ukrop.bandera@gmail.com>
2019-01-23 22:34:21 +0200

    34_1

--- app/src/main/java/com/ua/yuriihrechka/writeme/ProfileActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/writeme/ProfileActivity.java
@@ -3,10 +3,12 @@ package com.ua.yuriihrechka.writeme;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,7 +26,11 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView userProfileStatus;
     private Button sendMessageRequestButton;
 
+    private String currentState;
+    private String senderUserID;
+
     private DatabaseReference dbUserRef;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         dbUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        auth = FirebaseAuth.getInstance();
+        senderUserID = auth.getCurrentUser().getUid();
+
 
         receiverUserID = getIntent().getExtras().get("visit_user_id").toString();
 
@@ -90,9 +99,24 @@ public class ProfileActivity extends AppCompatActivity {
          userProfileStatus = (TextView)findViewById(R.id.visit_user_status);
          sendMessageRequestButton = (Button)findViewById(R.id.send_message_request_button);
 
+         currentState = "new";
+
     }
 
     private void manageChatRequest(){
+
+        if (senderUserID.equals(receiverUserID)){
+
+            sendMessageRequestButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }else {
+            sendMessageRequestButton.setVisibility(View.INVISIBLE);
+        }
 
     }
 }
